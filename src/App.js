978690938305c0d69b1cd267171099657aaac19e
Paterne <diedhioupaterne279@gmail.com>
2025-04-09 12:30:22 +0000

    hash fonctionnality

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import Home from "../src/pages/Home.js";
 import About from "../src/pages/About.js";
 import Contact from "../src/pages/Contact.js";
 import Projects from "../src/pages/Projects.js";
-import Skills from "../src/pages/Skills.js";
+import Skills from "./pages/Skills.js";
 import "./assets/css/main.css";
 import "./assets/css/about.css";
 import "./assets/css/projects.css";
@@ -12,16 +12,21 @@ import "./assets/css/contact.css";
 export default function App() {
   const hash = window.location.hash;
 
-  switch (hash) {
-    case "#about":
-      return About();
-    case "#projects":
-      return Projects();
-    case "#skills":
-      return Skills();
-    case "#contact":
-      return Contact();
-    default:
-      return Home();
+  if (hash.includes("about")) {
+    return About();
   }
+
+  if (hash.includes("projects")) {
+    return Projects();
+  }
+
+  if (hash.includes("skills")) {
+    return Skills();
+  }
+
+  if (hash.includes("contact")) {
+    return Contact();
+  }
+
+  return Home();
 }
--- src/main.js
+++ src/main.js
@@ -1,14 +1,14 @@
+//importer le fichier App.js
+import "./assets/css/main.css";
 import App from "./App.js";
 
-function renderApp() {
+document.addEventListener("DOMContentLoaded", () => {
   const app = document.querySelector("#app");
-  if (app) {
-    app.innerHTML = App();
+
+  function render() {
+    if (app) app.innerHTML = App();
   }
-}
 
-// Écouter l'événement de chargement de la page (DOM prêt)
-document.addEventListener("DOMContentLoaded", renderApp);
-
-// Écouter les changements de hash dans l'URL
-window.addEventListener("hashchange", renderApp);
+  window.addEventListener("hashchange", render);
+  render(); // initial
+});
